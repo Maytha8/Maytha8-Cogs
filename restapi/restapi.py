d406f56b6bdfad0775bdfc3d9df2301dbd35da57
Maytham <44939173+Maytha8@users.noreply.github.com>
2021-09-22 19:13:26 +0800

    remove commented listener

--- restapi/restapi.py
+++ restapi/restapi.py
@@ -11,7 +11,3 @@ class RestApi(commands.Cog):
         """Test command for REST API."""
         await ctx.send("REST API up and running!")
         await ctx.send(type(self).__module__)
-
-    # @commands.Cog.listener()
-    # async def on_message(self, message):
-    #     await message.add_reaction(":happy:")
