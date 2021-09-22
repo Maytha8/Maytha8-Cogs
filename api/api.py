42f0a32acb92e6957b180fa356f78c6c50fcb600
Maytham <44939173+Maytha8@users.noreply.github.com>
2021-09-23 07:06:47 +0800

    update

--- api/api.py
+++ api/api.py
@@ -1,4 +1,5 @@
-from redbot.core import commands, utils
+from redbot.core import commands
+from redbot.core.utils import get
 
 class api(commands.Cog):
     """My custom API cog"""
@@ -17,7 +18,7 @@ class api(commands.Cog):
     async def verifinit(self, ctx):
         verifmsg = await self.bot.get_channel(887951332915429376).fetch_message(890366650703028305)
         await verifmsg.clear_reactions()
-        await verifmsg.add_reaction(':StoodVerif:')
+        await verifmsg.add_reaction(get(self.bot.get_all_emojis(), name='StoodVerif'))
 
     @commands.Cog.listener()
     async def on_message(self, msg):
