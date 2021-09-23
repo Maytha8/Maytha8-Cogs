b83c31e7c0469fa55219597e73321d54c182da88
Maytham <44939173+Maytha8@users.noreply.github.com>
2021-09-23 18:45:32 +0800

    fix

--- invite/__init__.py
+++ invite/__init__.py
@@ -14,4 +14,4 @@ class Invite(commands.Cog):
     async def sendinvite(self, ctx, usr: discord.User):
         if self.bot.get_user(user.id):
             user = self.bot.get_user(user.id)
-            user.send("**We invite you to the Stood server.**\nThe Stood server is a study server dedicated to help you improve study and academical performance.\n\nJoin now! discord.gg/gpFR7sFcjC\n\nYou're not required to join, so you may decline the invite if you wish to do so.\n\nFor any issues or concerns, please DM <@!406762686261624832>.\n\nRegards, The Stood Team.")
+            await user.send("**We invite you to the Stood server.**\nThe Stood server is a study server dedicated to help you improve study and academical performance.\n\nJoin now! discord.gg/gpFR7sFcjC\n\nYou're not required to join, so you may decline the invite if you wish to do so.\n\nFor any issues or concerns, please DM <@!406762686261624832>.\n\nRegards, The Stood Team.")
