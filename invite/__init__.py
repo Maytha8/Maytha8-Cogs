4530b313a2709d45eb1d4e7f39f5f4235378aa43
Maytham <44939173+Maytha8@users.noreply.github.com>
2021-09-23 18:46:48 +0800

    fix variable names

--- invite/__init__.py
+++ invite/__init__.py
@@ -11,7 +11,7 @@ class Invite(commands.Cog):
         self.bot = bot
 
     @commands.command()
-    async def sendinvite(self, ctx, usr: discord.User):
+    async def sendinvite(self, ctx, user: discord.User):
         if self.bot.get_user(user.id):
-            user = self.bot.get_user(user.id)
-            await user.send("**We invite you to the Stood server.**\nThe Stood server is a study server dedicated to help you improve study and academical performance.\n\nJoin now! discord.gg/gpFR7sFcjC\n\nYou're not required to join, so you may decline the invite if you wish to do so.\n\nFor any issues or concerns, please DM <@!406762686261624832>.\n\nRegards, The Stood Team.")
+            usr = self.bot.get_user(user.id)
+            await usr.send("**We invite you to the Stood server.**\nThe Stood server is a study server dedicated to help you improve study and academical performance.\n\nJoin now! discord.gg/gpFR7sFcjC\n\nYou're not required to join, so you may decline the invite if you wish to do so.\n\nFor any issues or concerns, please DM <@!406762686261624832>.\n\nRegards, The Stood Team.")
