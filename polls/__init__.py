a8fb355380dbb31c32b4d874f09202d4dde957ed
Maytham <44939173+Maytha8@users.noreply.github.com>
2021-09-23 18:19:07 +0800

    add rating poll

--- polls/__init__.py
+++ polls/__init__.py
@@ -11,6 +11,13 @@ class Polls(commands.Cog):
 
     @commands.Cog.listener()
     async def on_message(self, msg):
-        if msg.channel.id == 890539259646345266 and "[POLL]" in msg.content:
-            await msg.add_reaction('✅')
-            await msg.add_reaction('❌')
+        if msg.channel.id == 890539259646345266:
+            if "[POLL]" in msg.content:
+                await msg.add_reaction('✅')
+                await msg.add_reaction('❌')
+            elif "[RATE]" in msg.content:
+                await msg.add_reaction('1')
+                await msg.add_reaction('2')
+                await msg.add_reaction('3')
+                await msg.add_reaction('4')
+                await msg.add_reaction('5')
