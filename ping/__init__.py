c41a4f38d0c29c40b1b865b47c8775337f19ec3d
Maytham <44939173+Maytha8@users.noreply.github.com>
2021-09-25 08:49:24 +0800

    [Ping] Create new cog

---
+++ ping/__init__.py
@@ -0,0 +1,18 @@
+from redbot.core import commands
+
+def setup(bot):
+    bot.add_cog(Ping(bot))
+
+class Ping(commands.Cog):
+    """Ping users on announcements."""
+
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.Cog.listener()
+    async def on_message(self, msg):
+        if "[PING]" in msg.content:
+            if msg.channel.id == 888355679469989918:
+                await self.bot.get_channel(888355679469989918).send("> @everyone <:this:891123206457544714>")
+            elif msg.channel.id == 891118316758925355:
+                await self.bot.get_channel(888355679469989918).send("> <@&891122442431524864> <:this:891123206457544714>")
