a8d723a718272b36b7bcd7ab30eb02ca60494f82
Maytham <44939173+Maytha8@users.noreply.github.com>
2021-09-25 11:07:12 +0800

    [Invite] Remove indent from message

--- invite/__init__.py
+++ invite/__init__.py
@@ -15,14 +15,14 @@ class Invite(commands.Cog):
         if self.bot.get_user(user.id):
             usr = self.bot.get_user(user.id)
             await usr.send("""**You are invited to join the Stood Server.**
-            The Stood server is a study server dedicated to help you improve study and academical performance.
-            You're not required to join, so you may decline the invite if you wish to do so.
+The Stood server is a study server dedicated to help you improve study and academical performance.
+You're not required to join, so you may decline the invite if you wish to do so.
 
-            **Early Access**
-            You are invited to become an Alpha Tester on the Stood Server.
-            Alpha Testers will recieve exclusive recognision, and will be invited in special VIP events.
+**Early Access**
+You are invited to become an Alpha Tester on the Stood Server.
+Alpha Testers will recieve exclusive recognision, and will be invited in special VIP events.
 
-            Join now! discord.gg/gpFR7sFcjC
+Join now! discord.gg/gpFR7sFcjC
 
-            For any issues or concerns, please DM <@!406762686261624832>.
-            Regards, The Stood Team.""")
+For any issues or concerns, please DM <@!406762686261624832>.
+Regards, The Stood Team.""")
