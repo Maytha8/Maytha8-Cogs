5a35a30cd13f804c5e52ba87eae25440d7557f0b
Maytham <44939173+Maytha8@users.noreply.github.com>
2021-09-25 11:04:21 +0800

    [Invite] Modify message

--- invite/__init__.py
+++ invite/__init__.py
@@ -14,4 +14,15 @@ class Invite(commands.Cog):
     async def sendinvite(self, ctx, user: discord.User):
         if self.bot.get_user(user.id):
             usr = self.bot.get_user(user.id)
-            await usr.send("**We invite you to the Stood server.**\nThe Stood server is a study server dedicated to help you improve study and academical performance.\n\nJoin now! discord.gg/gpFR7sFcjC\n\nYou're not required to join, so you may decline the invite if you wish to do so.\n\nFor any issues or concerns, please DM <@!406762686261624832>.\n\nRegards, The Stood Team.")
+            await usr.send("""**You are invited to join the Stood Server.**
+            The Stood server is a study server dedicated to help you improve study and academical performance.
+            You're not required to join, so you may decline the invite if you wish to do so.
+
+            **Early Access**
+            You are invited to become an Alpha Tester on the Stood Server.
+            Alpha Testers will recieve exclusive recognision, and will be invited in special VIP events.
+
+            Join now! discord.gg/gpFR7sFcjC
+
+            For any issues or concerns, please DM <@!406762686261624832>.
+            Regards, The Stood Team.""")
